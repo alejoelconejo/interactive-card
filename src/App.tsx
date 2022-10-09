d9530812703015e4f62ddd372267fd77850263d1
Alejo Janin <alejojanin@gmail.com>
2022-10-09 09:45:00 -0300

    Add styles to form

--- src/App.tsx
+++ src/App.tsx
@@ -9,21 +9,53 @@ const App = () => {
         <form>
           <label>
             Cardholder name
-            <input placeholder='e.g. Jane Appleseed' type={'text'} />
+            <input
+              placeholder='e.g. Jane Appleseed'
+              name='name'
+              type={'text'}
+            />
           </label>
           <label>
             Card number
-            <input placeholder='e.g. 1234 5678 9123 0000' type={'number'} />
+            <input
+              placeholder='e.g. 1234 5678 9123 0000'
+              name='card-number'
+              type={'text'}
+              inputMode='numeric'
+            />
           </label>
-          <label>
-            Exp. Date (MM/YY)
-            <input placeholder='MM' type={'number'} />
-            <input placeholder='YY' type={'number'} />
-          </label>
-          <label>
-            CVC
-            <input placeholder='e.g. 123' type={'number'} />
-          </label>
+          <div className='form-horizontal'>
+            <label>
+              Exp. Date (MM/YY)
+              <div className='inputs-date'>
+                <input
+                  className='input-date'
+                  name='exp-month'
+                  placeholder='MM'
+                  type={'text'}
+                  inputMode='numeric'
+                />
+                <input
+                  className='input-date'
+                  name='exp-year'
+                  placeholder='YY'
+                  type={'text'}
+                  inputMode='numeric'
+                />
+              </div>
+            </label>
+            <label>
+              CVC
+              <input
+                className='input-cvc'
+                name='cvc'
+                placeholder='e.g. 123'
+                type={'text'}
+                inputMode='numeric'
+              />
+            </label>
+          </div>
+
           <button>Confirm</button>
         </form>
       </section>
